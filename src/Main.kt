7b9081dda6952833c9dd768e8cac64ffe83b7e18
safitriks <safitri.suardini@gmail.com>
2024-03-02 11:27:27 +0700

    Add Feature Circle

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,21 @@ class TriangleArea(){
     fun trianglePerimeter(width: Int) = width*3
 }
 
+class Circle(private var radius: Double) {
+
+    fun calculateArea(): Double{
+        return Math.PI * radius * radius
+    }
+
+    fun calculateCircumfence(): Double{
+        return Math.PI * 2 * radius
+    }
+}
+
 fun main() {
+    val circle = Circle(10.0)
+    println("Area : ${circle.calculateArea()}")
+    println("Circumfence : ${circle.calculateCircumfence()}")
     println(TriangleArea().triangeArea(2,2))
     println(TriangleArea().trianglePerimeter(2))
 }
